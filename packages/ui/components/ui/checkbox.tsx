8917178bf974ac365428e03faf861a2900fdda0e
pkupuk <pukpuk.tw@gmail.com>
2026-01-13 12:10:52 +0800

    chore: update dependencies and UI components
    - Update root package.json
    - Update pnpm-lock.yaml
    - Update checkbox UI component

--- packages/ui/components/ui/checkbox.tsx
+++ packages/ui/components/ui/checkbox.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as CheckboxPrimitive from '@radix-ui/react-checkbox';
-// Tree-shakable Lucide imports
+import { CheckIcon } from 'lucide-react';
 
 import { cn } from '@/lib/utils';
 
